04f28a1ce904f97dd7c1af81e1298a0cffe774f2
achyut <achyutchaudhary47@gmail.com>
2026-04-22 22:13:22 +0530

    Properties

--- src/ImmutuableClass/Demo.java
+++ src/ImmutuableClass/Demo.java
@@ -1,5 +1,27 @@
 package ImmutuableClass;
 
 public class Demo {
+    static void main() {
+        Student s1=new Student(28,"Achyut");
+        System.out.println(s1.getAge());
+        System.out.println(s1.getName());
+    }
+}
 
+final class Student{
+    private final int age;
+    private final String name;
+    Student(int age,String name){
+        this.age=age;
+        this.name=name;
+    }
+    //getters
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/ImmutuableClass/Rules.md
+++ src/ImmutuableClass/Rules.md
@@ -2,3 +2,4 @@ Immutable class are those classes which value does not changes
 mark my class as final
 mark my variables as private and final
 no setters
+immutable class cannot be extended
