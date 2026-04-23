66f37566ede78f7b5491f81ceee4c98b491ebc64
achyut <achyutchaudhary47@gmail.com>
2026-04-23 06:15:19 +0530

    Student class is not purely immutable

--- src/ImmutuableClass/Demo.java
+++ src/ImmutuableClass/Demo.java
@@ -2,18 +2,23 @@ package ImmutuableClass;
 
 public class Demo {
     static void main() {
-        Student s1=new Student(28,"Achyut");
+        College college=new College("IIT G","Assam");
+        Student s1=new Student(28,"Achyut",college);
         System.out.println(s1.getAge());
         System.out.println(s1.getName());
+        System.out.println(college.name);
+
     }
 }
 
 final class Student{
     private final int age;
     private final String name;
-    Student(int age,String name){
+    private final College college;
+    Student(int age,String name,College college){
         this.age=age;
         this.name=name;
+        this.college=college;
     }
     //getters
 
@@ -23,5 +28,17 @@ final class Student{
 
     public String getName() {
         return name;
+    }
+    public College getCollege() {
+        return college;
+    }
+}
+//mutuable
+class College{
+    String name;
+    String address;
+    College(String name,String address){
+        this.name=name;
+        this.address=address;
     }
 }
