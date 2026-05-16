95b851a73a253648e96a4e12fdc143ac02261945
achyut <achyutchaudhary47@gmail.com>
2026-05-16 18:21:53 +0530

    thread is non deterministic

--- src/Multithreading/Demo5.java
+++ src/Multithreading/Demo5.java
@@ -1,4 +1,28 @@
 package Multithreading;
 
 public class Demo5 {
+    static void main() {
+        // 1 to 100(even numbers)
+        Thread t1= new Thread(()->{
+            for(int i=1;i<=100;i++) {
+                if(i%2==0) {
+                    System.out.println("T1 "+i);
+                }
+            }
+        });
+
+        //1 to 100(odd number)
+        Thread t2= new Thread(()->{
+            for(int i=1;i<=100;i++) {
+                if(i%2!=0) {
+                    System.out.println("T2 "+i);
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
 }
+
+//thread is non deterministic
