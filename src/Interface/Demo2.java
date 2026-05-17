e437d19a2de0aa5b6e341b473a667f674762aa1a
achyut <achyutchaudhary47@gmail.com>
2026-05-17 08:24:11 +0530

    abstract class property

--- src/Interface/Demo2.java
+++ src/Interface/Demo2.java
@@ -1,4 +1,29 @@
 package Interface;
+import java.util.*;
 
 public class Demo2 {
+    static void main() {
+        Car1 c = new BlackThar(){
+
+        };
+
+    }
+}
+
+
+interface Car1{
+    void drive();
+}
+/*
+when class does not want to override the interface functionality , then we
+have to make the class as the abstract method
+ */
+abstract class Thar1 implements Car1{
+    abstract public void drive();
+}
+class BlackThar extends Thar1{
+    @Override
+    public void drive() {
+
+    }
 }
