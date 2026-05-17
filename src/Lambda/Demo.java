2a1be7f27b6c51548a30eff27ae56a68e71ba406
achyut <achyutchaudhary47@gmail.com>
2026-05-17 08:26:09 +0530

    lambda

--- src/Lambda/Demo.java
+++ src/Lambda/Demo.java
@@ -1,4 +1,24 @@
 package Lambda;
 
+import java.util.*;
+
+import java.util.*;
+import java.util.List;
+
 public class Demo {
+    static void main() {
+
+    }
+}
+
+class Student{
+    String name;
+    int rollNo;
+    int marks;
+
+    public Student(String name, int rollNo, int marks) {
+        this.name = name;
+        this.rollNo = rollNo;
+        this.marks = marks;
+    }
 }
