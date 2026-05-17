1efd2cc6abda46e2a770e9ece8a76800bc716444
achyut <achyutchaudhary47@gmail.com>
2026-05-17 10:30:06 +0530

    variables in the interfaces

--- src/Interface/Demo4.java
+++ src/Interface/Demo4.java
@@ -12,10 +12,19 @@ public class Demo4 {
         there fore to access the r1.fun() we have to create the fun() in the
         interface
          */
+        r1.fun();
 
     }
 }
 //variables inside interfaces
+/*
+where we use this property ?
+we use this property in the production grade application where we have to use
+ the constant values and then we use interfaces and use it
+ we can same achieve the  with the class only but the issue it we have to
+ declare all the static final and public things over there that we use the
+ interfaces instead
+ */
 interface MathConstant{
     /*
     by default in the interfaces whenever we make the variable it should be
@@ -25,10 +34,11 @@ interface MathConstant{
      */
     double PI_VALUE=3.14;
     int VALUE =10;
-
+    void fun();
 }
 class Random implements MathConstant{
-    void fun(){
+    @Override
+    public void fun(){
         System.out.println(PI_VALUE);
     }
 }
