f7d8e7dfcaf0b5dcc6df7d5ba24f45fb3ccdbbef
achyut <achyutchaudhary47@gmail.com>
2026-05-17 10:08:29 +0530

    polymorphism

--- src/Interface/Demo3.java
+++ src/Interface/Demo3.java
@@ -1,4 +1,27 @@
 package Interface;
 
 public class Demo3 {
+    static void main() {
+        Payment p=new CreditCard();
+        p.pay();
+    }
+}
+
+//polymorphism
+interface Payment{
+    void pay();
+}
+
+class CreditCard implements Payment{
+    @Override
+    public void pay() {
+        System.out.println("Paying via credit card");
+    }
+}
+
+class DebitCard implements Payment{
+    @Override
+    public void pay() {
+        System.out.println("Paying via debit card");
+    }
 }
