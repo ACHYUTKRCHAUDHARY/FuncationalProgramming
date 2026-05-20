19111157b5acce256983ecb0010ef78c49e1053a
achyut <achyutchaudhary47@gmail.com>
2026-05-21 02:40:25 +0530

    static variable

---
+++ src/Final/Demo.java
@@ -0,0 +1,22 @@
+package Final;
+
+class Student{
+    String name;
+    int age;
+    int rollNumber;
+    static String CollegeName;
+
+    Student(String name,int age,int rollNumber){
+        this.name=name;
+        this.age=age;
+        this.rollNumber=rollNumber;
+    }
+
+}
+
+public class Demo {
+    public static void main(String[] args) {
+        Student s1=new Student("Achyut")
+    }
+}
+
