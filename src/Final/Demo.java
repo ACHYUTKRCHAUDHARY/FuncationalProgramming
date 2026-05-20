e7f7bd3e724e7032ce31018d9219ea16f511348e
achyut <achyutchaudhary47@gmail.com>
2026-05-21 02:42:39 +0530

    static blocks nd variables

--- src/Final/Demo.java
+++ src/Final/Demo.java
@@ -11,12 +11,20 @@ class Student{
         this.age=age;
         this.rollNumber=rollNumber;
     }
-
+    //static block
+    static{
+        CollegeName="Bpit";
+    }
 }
 
 public class Demo {
     public static void main(String[] args) {
-        Student s1=new Student("Achyut")
+        Student s1=new Student("Achyut",20,101);
+        Student s2=new Student("Aditiya",28,102);
+
+        Student.CollegeName="Bpit";
+        System.out.println(s1.name+" , "+s1.age+" , "+s1.rollNumber+" , "+ Student.CollegeName);
+        System.out.println(s2.name+" , "+s2.age+" , "+s2.rollNumber+" , "+ Student.CollegeName);
     }
 }
 
