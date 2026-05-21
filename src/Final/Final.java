5962a12e10a9fadecda8c04def95418ebfec22bd
achyut <achyutchaudhary47@gmail.com>
2026-05-21 15:23:45 +0530

    one way of putting the values

---
+++ src/Final/Final.java
@@ -0,0 +1,13 @@
+package Final;
+
+public class Final {
+    static void main() {
+        Random r1=new  Random();
+        System.out.println(r1.PI);
+    }
+}
+
+class Random{
+    final double PI=3.14;
+
+}
